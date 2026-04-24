a881a7b858c0aa5de314247b77f1d7446f09c259
Narwer971 <Narwer971@gmail.com>
2026-04-24 10:33:40 -0400

    Add files via upload

---
+++ Site_nsi_EL.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+	alert("bonjour")
+
+ 	let nom = prompt("comment t'apelle tu ?");localStorage.getItem("nom");
+
+  	alert ("bonjour " + nom)
+
+
+});
+
